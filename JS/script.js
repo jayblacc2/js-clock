10d7e8c34c6882b5b83c874f94dda518a7ac72aa
Jide Johnson <jideman2k@yandex.ru>
2022-08-19 18:02:24 +0400

    random color range

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,17 @@
-let currentHr = document.querySelector("#hr");
-let currentMin = document.querySelector("#min");
-let currentSec = document.querySelector("#sec");
+let currentHr = document.querySelector('#hr');
+let currentMin = document.querySelector('#min');
+let currentSec = document.querySelector('#sec');
+
+const colors = [
+  '#1363DF',
+  '#827397',
+  '#68A7AD',
+  '#8479E1',
+  '#5B7DB1',
+  '#C3B091',
+];
+
+console.log(randColor());
 
 function runClock() {
   const currentDate = new Date();
@@ -9,20 +20,17 @@ function runClock() {
   let minutes = currentDate.getMinutes();
   let seconds = currentDate.getSeconds();
 
-  randomBgColor(seconds)
+  randomBgColor(seconds, colors);
 
   if (hours < 10) {
-    hours = "0" + hours.toString();
+    hours = '0' + hours.toString();
   }
   if (minutes < 10) {
-    minutes = "0" + minutes.toString();
+    minutes = '0' + minutes.toString();
   }
   if (seconds < 10) {
-    seconds = "0" + seconds.toString();
+    seconds = '0' + seconds.toString();
   }
-
-
-
 
   currentHr.innerText = hours;
   currentMin.innerText = minutes;
@@ -31,18 +39,18 @@ function runClock() {
 
 let interval = setInterval(runClock, 1000);
 
-
-function randomBgColor(seconds) {
-  if(+seconds === 00) {
-  const colors = ['#1363DF', '#827397','#68A7AD', '#8479E1', '#5B7DB1', '#C3B091']
-    const randomColor =colors[Math.floor(Math.random() *  colors.length)  ]
-    console.log(randomColor)
-    document.body.style['background'] = `${randomColor}`
+function randomBgColor(seconds, arrColors) {
+  const randomColor = arrColors[Math.floor(Math.random() * arrColors.length)];
+  if (+seconds === 00) {
+    // document.body.style['background'] = `${randomColor}`;
+    document.body.style['background'] = randColor();
   }
+}
 
- }
-
-
-
-
-
+function rangeInt(min, max) {
+  const randInt = Math.floor(Math.random() * (max - min - 1)) + min;
+  return randInt;
+}
+function randColor() {
+  return `rgb(${rangeInt(0, 255)}, ${rangeInt(0, 255)}, ${rangeInt(0, 255)})`;
+}
